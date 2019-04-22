68c7bbaaffb147fa7d3b6255d69d507dcb6f9c70
Kelsey Bajis <bajis.simple.haven@gmail.com>
2019-04-22 04:57:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,11 @@
 def line(katz_deli)
   if katz_deli.empty?
     puts "the line is currently empty."
+  else
+    current_line = "The line is currently:"
+    katz_deli.each_with_index(1) do |person, index|
+      current_line << "#{index}. #{person}"
   end
+  puts current_line
+end
 end
