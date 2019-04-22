4c0cbd5b0058a13192a02f1df6ad75986b321d03
Kelsey Bajis <bajis.simple.haven@gmail.com>
2019-04-22 04:55:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(katz_deli)
-  if katz_deli == 0
-    puts "The line is currently empty."
+  if katz_deli.empty?
+    puts "the line is currently empty."
   end
 end
